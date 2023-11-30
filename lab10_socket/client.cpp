f91ad23461df1026859fed57196dd3ea85256945
Naman <23m2109@iitb.ac.in>
2023-12-01 05:05:46 +0530

    messages and comments

--- lab10_socket/client.cpp
+++ lab10_socket/client.cpp
@@ -61,15 +61,14 @@ int main(int argc, char *argv[])
         }
 
     if (requestType == "new")
-    // if (strcmp(requestType, "done") == 0)
     {
         
         send(clientSocket, "new", 3, 0);
+        cout << "Request Type Send " <<endl;
         
         char buffer_status_recieved[25];
         ssize_t sizeReceived = recv(clientSocket, &buffer_status_recieved, sizeof(buffer_status_recieved), 0);
 
-        cout << buffer_status_recieved << endl;
 
         // Read the content of the source code file
         ifstream sourceFile(sourceFileOrRquestId,ios::binary | ios::ate);
@@ -90,9 +89,10 @@ int main(int argc, char *argv[])
 
         send(clientSocket,&fileSize,sizeof(fileSize),0);
         
-        ssize_t sizeReceived_after_sending_size = recv(clientSocket, &buffer_after_sending_size, sizeof(buffer_after_sending_size), 0);
-        cout << buffer_after_sending_size <<endl;
+        cout << "File Size "<<fileSize << " sent to the server"<<endl;
 
+        ssize_t sizeReceived_after_sending_size = recv(clientSocket, &buffer_after_sending_size, sizeof(buffer_after_sending_size), 0);
+        
         // read contents of sourceFile linebyline until EOF and copy it to sourceCodeContent
         string sourceCodeContent((istreambuf_iterator<char>(sourceFile)),
                                  istreambuf_iterator<char>());
@@ -102,12 +102,11 @@ int main(int argc, char *argv[])
         send(clientSocket, request.c_str(), request.size(), 0);
         string eofMarker = "EOF";
         send(clientSocket, eofMarker.c_str(), eofMarker.size(), 0);
-        cout << "file sent "<<endl;
+        cout << "Source Code file sent to the server "<<endl;
+
 
         char buffer_after_file_send[13];
         ssize_t sizeReceived_afetrFileSend = recv(clientSocket, &buffer_after_file_send, sizeof(buffer_after_file_send), 0);
-        cout<<buffer_after_file_send<<endl;
-
 
         // Receive and display the server response
         char buffer[1024];
@@ -120,7 +119,7 @@ int main(int argc, char *argv[])
         else
         {
             buffer[bytesRead] = '\0';
-            cout << buffer << endl;
+            cout << "ID Recieved is "<< buffer << endl;
         }
 
         close(clientSocket);
@@ -128,39 +127,12 @@ int main(int argc, char *argv[])
 
     else if (requestType == "status")
     {
-        // while (true)
-        // {
-            // int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-            // if (clientSocket == -1)
-            // {
-            //     perror("Socket creation error");
-            //     return 1;
-            // }
-
-            // // Connect to the server
-            // // Data structure for socket
-            // struct sockaddr_in serverAddress;
-            // serverAddress.sin_family = AF_INET;
-            // serverAddress.sin_port = htons(port);
-
-            // if (inet_pton(AF_INET, serverIP.c_str(), &serverAddress.sin_addr) <= 0)
-            // {
-            //     perror("Invalid server address");
-            //     close(clientSocket);
-            //     return 1;
-            // }
-            // if (connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1)
-            // {
-            //     perror("Connection error");
-            //     close(clientSocket);
-            //     return 1;
-            // }
-
+            //Request type sending
             string statusRequest = requestType + "-" + sourceFileOrRquestId;
             send(clientSocket, statusRequest.c_str(), statusRequest.size(), 0);
-            // sleep(1);
-            // send(clientSocket, sourceFileOrRquestId.c_str(), sourceFileOrRquestId.size(), 0);
 
+            cout<<"Status Request For "<< sourceFileOrRquestId<<" sent to the server"<<endl;
+     
             char buffer[1024];
             memset(buffer,0,sizeof(buffer));
             ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
@@ -172,12 +144,12 @@ int main(int argc, char *argv[])
             else
             {
                 buffer[bytesRead] = '\0';
-                cout << buffer << endl;
+                cout << "Request is "<< buffer << endl;
             }
 
             if (string(buffer)== "done")
             {
-                cout << "your grading is done"<<endl;
+                cout << "your grading is done" <<endl;
                 char response[1024];
                 memset(response,0,sizeof(response));
                 ssize_t bytesRead = recv(clientSocket, response, sizeof(response), 0);
@@ -199,10 +171,10 @@ int main(int argc, char *argv[])
             else
             {
                 close(clientSocket);
-                // return 0;
+                
                 sleep(2);
             }
-        // }
+        
     }
 
     else
